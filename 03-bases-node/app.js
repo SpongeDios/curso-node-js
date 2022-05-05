5d3f3c6dab72d2ef5c8e6465091d00e757e051ba
Hector Alvarado <hectoralvaradocampos83@gmail.com>
2022-05-05 08:02:22 -0400

    working with dependency and package json

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,8 +1,20 @@
 const {crearArchivo} = require('./helpers/multiplicar')
+const argv = require('yargs').argv
 
 console.clear() // limpia la consola
-const base = 4;
 
-crearArchivo(base)
-    .then(nombreArchivo => console.log(nombreArchivo))
-    .catch(err => console.log(err))
+// const[, , arg3] = process.argv
+// const[, base = 5] = arg3.split('=')
+// console.log(process.argv);
+
+
+// const base = 4;
+// console.log(base);
+
+
+// crearArchivo(base)
+//     .then(nombreArchivo => console.log(nombreArchivo))
+//     .catch(err => console.log(err))
+
+console.log(argv);
+console.log('Base: yargs', argv.base);
